13d2abd9c2fefd9e0e476c7d275c03d3314c0307
Frommflin <freakyfrommelin@outlook.com>
2023-07-17 17:53:29 +0200

    Add project files.

---
+++ DyviksnäsGård/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace DyviksnäsGård.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
