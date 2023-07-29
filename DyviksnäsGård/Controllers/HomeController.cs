8c3187de3d34ec55d15163be832bfe40f078fde8
Klara Frommelin <FreakyFrommelin@outlook.com>
2023-07-29 23:35:30 +0200

    Adding About page

--- DyviksnäsGård/Controllers/HomeController.cs
+++ DyviksnäsGård/Controllers/HomeController.cs
@@ -18,7 +18,7 @@ namespace DyviksnäsGård.Controllers
             return View();
         }
 
-        public IActionResult Privacy()
+        public IActionResult About()
         {
             return View();
         }
